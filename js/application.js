dd4c42c7871d506865e3232198dc66a7c382d234
AmmarAliShah <syedammarali24@gmail.com>
2015-04-21 11:31:46 +0500

    inex.html Header experiments

--- js/application.js
+++ js/application.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   $('h1').on("mouseenter", function(){
-    $(this).toggleClass('animated flash');
+    $('span').toggleClass('animated flash');
+    $( 'span' ).css( "color", "red" );
+  });
+  $('h1').on("mouseleave", function(){
+    $( 'span' ).css( "color", "black" );
   });
 });
