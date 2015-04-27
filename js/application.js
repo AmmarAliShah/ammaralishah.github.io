3b8d880165b2bef8ae9acea1d94123e4f7ac5a6f
AmmarAliShah <syedammarali24@gmail.com>
2015-04-27 12:13:07 +0500

    checkout

--- js/application.js
+++ js/application.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   $('h1').on("mouseenter", function(){
-    $('span').toggleClass('animated flash');
-    $( 'span' ).css( "color", "red" );
+    $( '.red' ).css( "color", "red" );
   });
   $('h1').on("mouseleave", function(){
-    $( 'span' ).css( "color", "black" );
+    $( '.red' ).css( "color", "black" );
   });
 });
