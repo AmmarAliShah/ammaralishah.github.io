721dd312bc7b54e048c142b7544849d57629fad9
AmmarAliShah <syedammarali24@gmail.com>
2015-06-08 12:17:56 +0500

    Makeover

--- js/application.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('h1').on("mouseenter", function(){
-    $( '.red' ).css( "color", "red" );
-  });
-  $('h1').on("mouseleave", function(){
-    $( '.red' ).css( "color", "black" );
-  });
-});
